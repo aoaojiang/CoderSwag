58a2e83502a1a45b043a45b0df494dc436f547ee
Jiang Pei <18917944949@189.cn>
2019-02-01 09:49:06 +0800

    first commit

---
+++ app/src/main/java/com/example/coderswag/Model/Category.kt
@@ -0,0 +1,7 @@
+/**
+ * <p>Create Time: 2019年02月01日 09:20  </p>
+ * <p>@author JIANG Pei     </p>
+ **/
+package com.example.coderswag.Model
+
+class Category(val title: String, val image: String)
---
+++ app/src/main/java/com/example/coderswag/Model/Product.kt
@@ -0,0 +1,7 @@
+/**
+ * <p>Create Time: 2019年02月01日 09:21  </p>
+ * <p>@author JIANG Pei     </p>
+ **/
+package com.example.coderswag.Model
+
+class Product(val title: String, val price: String, val image: String)
